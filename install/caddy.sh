cdc450a52d2d7dca16bdd356f132555055796716
Nette Mashewske <33563347+nmashewske@users.noreply.github.com>
2020-11-24 16:38:09 -0500

    Result write to sqlite db, added caddy install script

---
+++ install/caddy.sh
@@ -0,0 +1,4 @@
+echo "-------Downloading Caddy----------"
+curl -L https://github.com/caddyserver/caddy/releases/download/v2.2.1/caddy_2.2.1_linux_amd64.tar.gz --output caddy.tar.gz -#
+tar -xzf caddy.tar.gz caddy
+mv caddy ../caddy
