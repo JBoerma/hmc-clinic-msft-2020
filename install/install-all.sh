60fb1504f8a39136910b2d5740d9b6b711904f95
Nette Mashewske <33563347+nmashewske@users.noreply.github.com>
2020-11-26 10:15:58 -0500

    Improved installer to fix firefox issues, pip issues

--- install/install-all.sh
+++ install/install-all.sh
@@ -1,3 +1,8 @@
 ./caddy.sh
 ./nginx-quic.sh
 ./nginx-quiche.sh
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+folder=$(ls ~/.cache/ms-playwright/* -d | grep firefox | sort -r | head -n 1)
+mkdir -p $folder/firefox/distribution
+cp policies.json $folder/firefox/distribution/policies.json
