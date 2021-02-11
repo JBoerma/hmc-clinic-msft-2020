dd445fa79354d588e0f91b6e22560e407ac66798
Nette Mashewske <33563347+nmashewske@users.noreply.github.com>
2021-02-11 11:44:58 -0500

    Switched back to sync, updated installers

--- install/install-all.sh
+++ install/install-all.sh
@@ -2,17 +2,12 @@
 ./caddy.sh
 ./nginx-quic.sh
 ./nginx-quiche.sh
-sudo wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
-sudo apt install -y openlitespeed
-sudo chown -R $USER /usr/local/lsws/conf
-\cp -r lsws /usr/local/
+./lsws.sh
 # Install playwright
 sudo apt install -y python3-pip iproute2 libdbus-glib-1-2
 pip3 install -r requirements.txt
-playwright install
+python3 -m playwright install
 # Configure firefox
-folder=$(ls ~/.cache/ms-playwright/* -d | grep firefox | sort -r | head -n 1)
-mkdir -p $folder/firefox/distribution
-cp policies.json $folder/firefox/distribution/policies.json
+./firefox.sh
 # Download and install Edge
 ./edge.sh
