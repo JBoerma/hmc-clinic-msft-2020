6a5df675e4577d6478f14441e38582165cb60ac2
Ubuntu <clinic@Client-VM.arwmfc444k0e3ko1nhv1gjrbff.xx.internal.cloudapp.net>
2021-02-20 22:34:23 +0000

    Added changes to client install - need to run python first

--- install/python-install.sh
+++ install/python-install.sh
@@ -1,4 +1,5 @@
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev curl libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev
+sudo apt update
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev curl libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev make gcc
 curl -L https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz --output python.tgz
 tar -xf python.tgz
 folder=$(ls | grep P)
