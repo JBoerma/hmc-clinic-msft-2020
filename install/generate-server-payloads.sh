adee08dcfa12be8753c8eefc85138b223d79186b
kekejin <kjin@hmc.edu>
2021-04-09 03:26:34 +0000

    add small/medium/large payloads local

--- install/generate-server-payloads.sh
+++ install/generate-server-payloads.sh
@@ -14,17 +14,17 @@ done
 
 for i in {400..419}
 do
-echo "<img  src='https://placekitten.com/${i}/${i}' width='${i}' height='${i}' alt=''>"
+echo "<img src='assets/cats_${i}.jpg' width='${i}' height='${i}' alt=''>"
 done > $SERVERDIR/small.html
 
 for i in {400..449}
 do
-echo "<img  src='https://placekitten.com/${i}/${i}' height='${i}' alt=''>"
+echo "<img src='assets/cats_${i}.jpg' height='${i}' alt=''>"
 done > $SERVERDIR/medium.html
 
 for i in {400..499}
 do
-echo "<img  src='https://placekitten.com/${i}/${i}' height='${i}' alt=''>"
+echo "<img  src='assets/cats_${i}.jpg height='${i}' alt=''>"
 done > $SERVERDIR/large.html
 
 for i in {400..499}
